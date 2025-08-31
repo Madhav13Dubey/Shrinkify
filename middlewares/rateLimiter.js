8ed0e466a3f3cc96d07932ae22d208e731b3dd17
Madhav Dubey <154184125+Madhav13Dubey@users.noreply.github.com>
2025-08-31 11:48:05 +0530

    Update rateLimiter.js

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 // create a limiter that allows 5 requests per 1 minutes from a single IP address //
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minutes
+  windowMs: 1 * 60 * 1000, // conversion from minutes --> 60 seconds --> microseconds // 
   max: 5, // limit each IP to 5 requests per window
   message: 'Too many requests from this IP, please try again later.',
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
